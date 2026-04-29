ef66616bb5a53d762ee072b6bbfd23bc72693ea6
Alessio Bifulco <alessio.bifulco@studio.unibo.it>
2026-04-29 10:36:26 +0200

    feat: ex 3

--- src/main/scala/ex3/Solitaire.scala
+++ src/main/scala/ex3/Solitaire.scala
@@ -1,15 +1,44 @@
 package ex3
 
 object Solitaire extends App:
-  def render(solution: Seq[(Int, Int)], width: Int, height: Int): String =
-    val reversed = solution.reverse
+  private def render(solution: Seq[(Int, Int)], width: Int, height: Int): String =
     val rows =
-      for y <- 0 until height
-          row = for x <- 0 until width
-          number = reversed.indexOf((x, y)) + 1
-          yield if number > 0 then "%-2d ".format(number) else "X  "
+      for y <- 1 to height
+          row = for x <- 1 to width
+                    number = solution.indexOf((x, y)) + 1
+          yield if number > 0 then "%-3d".format(number) else "X  "
       yield row.mkString
     rows.mkString("\n")
 
+  private val moves: Seq[(Int, Int)] = Seq(
+    (-3, 0), (3, 0), (0, -3), (0, 3),
+    (-2, -2), (-2, 2), (2, -2), (2, 2)
+  )
 
-  println(render(solution = Seq((0, 0), (2, 1)), width = 3, height = 3))
+  private def placeMarks(board: (Int, Int)): Iterable[Seq[(Int, Int)]] =
+    val (w, h) = board
+    val center = ((w + 1) / 2, (h + 1) / 2)
+
+    def isValid(mark: (Int, Int), marks: Seq[(Int, Int)]): Boolean =
+      val (x, y) = mark
+      x >= 1 && x <= w &&
+        y >= 1 && y <= h &&
+        !marks.contains(mark)
+
+    def place(n: Int): Iterable[Seq[(Int, Int)]] = n match
+      case 1 => LazyList(Seq(center))
+      case _ =>
+        for
+          marks <- place(n - 1)
+          (x, y) = marks.last
+          (dx, dy) <- moves
+          mark = (x + dx, y + dy)
+          if isValid(mark, marks)
+        yield
+          marks :+ mark
+
+    place(w * h)
+
+  for sol <- placeMarks((5, 5)) do
+    println(render(solution = sol, width = 5, height = 5))
+    println("---")
